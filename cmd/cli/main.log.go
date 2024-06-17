953ef11426aa8a971f5e7d3d9a557c5f17124578
nhh57 <nguyenhoanghai0507@gmail.com>
2024-06-17 21:00:48 +0700

    log

---
+++ cmd/cli/main.log.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	// sugar := zap.NewExample().Sugar()
+	// sugar.Infof("Hello name:%s, age:%d ", "tipsGo", 40) // like fmt.Printf(format, a)
+
+	encoder := getEncoderLog()
+	sync := getWriterSync()
+	core := zapcore.NewCore(encoder, sync, zapcore.InfoLevel)
+	logger := zap.New(core, zap.AddCaller())
+
+	logger.Info("Info log", zap.Int("line", 1))
+	logger.Error("Error log", zap.Int("line", 2))
+}
+
+func getEncoderLog() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	//
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	// ts -> time
+	encoderConfig.TimeKey = "time"
+	// from info INFo
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	// "caller":"cli/main.log.go:24"
+	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	return zapcore.NewJSONEncoder(encoderConfig)
+}
+
+func getWriterSync() zapcore.WriteSyncer {
+	file, err := os.OpenFile("./log/log.txt", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	syncFile := zapcore.AddSync(file)
+	syncConsole := zapcore.AddSync(os.Stderr)
+	return zapcore.NewMultiWriteSyncer(syncConsole, syncFile)
+}
