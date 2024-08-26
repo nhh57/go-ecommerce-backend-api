47b867182ed590769dae1a68ab559107ada0d102
nhh57 <nguyenhoanghai0507@gmail.com>
2024-08-26 16:45:43 +0700

    Connect Mysql Pool

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,5 +1,53 @@
 package initialize
 
-func InitMysql(){
-	
+import (
+	"fmt"
+	"time"
+
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/nhh57/go-ecommerce-backend-api/global"
+)
+
+func checkErrorPanic(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+	}
+}
+
+func InitMysql() {
+	m := global.Config.Mysql
+	dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+	checkErrorPanic(err, "InitMysql initialization error")
+	global.Logger.Info("Initializing MYSQL Successfully")
+	global.Mdb = db
+	// set Pool
+	SetPool()
+}
+
+func SetPool() {
+
+	m := global.Config.Mysql
+	sqlDb, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Println("mysql error:: %s::", err)
+	}
+	sqlDb.SetConnMaxIdleTime(time.Duration(m.MaxIdleConns))
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDb.SetConnMaxIdleTime(time.Duration(m.ConnMaxLifetime))
+}
+
+func migrateTables() {
+	err:= global.Mdb.AutoMigrate(
+		$po.User{},
+		$po.Role{}
+	)
+	if()
 }
